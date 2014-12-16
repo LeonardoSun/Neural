f4023bd86f989597c7f83fb2df84a4907e181eaa
sunqinghao <sunqinghao@sunqinghao-desktop>
2014-12-16 09:46:32 +0800

    Modified:	add event abstract class.

---
+++ Source/event_args.py
@@ -0,0 +1,13 @@
+# encoding:utf-8
+'''
+Created on 2014年12月16日
+
+@author: sunqinghao
+'''
+
+class EventArgs(object):
+    pass
+    #
+    # Static Fields
+    #
+    #public static readonly EventArgs Empty;
---
+++ Source/event_handler.py
@@ -0,0 +1,11 @@
+# encoding:utf-8
+'''
+Created on 2014年12月16日
+
+@author: sunqinghao
+'''
+
+#public delegate void EventHandler (object sender, EventArgs e);
+
+class EventHandler(object):
+    pass
